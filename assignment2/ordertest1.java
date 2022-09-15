a60e955feb238538eb630a5abfe2a9f8d5c6309f
brando allen <111488269+badong0@users.noreply.github.com>
2022-09-15 16:28:24 +0800

    Create ordertest1.java
    this is were you test (main) if you can connect the class in the order.java(main)

---
+++ assignment2/ordertest1.java
@@ -0,0 +1,7 @@
+public class ordertest1 {
+    public static void main(String[] args) {
+
+        // Character Class
+        ordertest1 burger = new dishlist("Burger " + 99.9);
+        burger.sayTheDish();
+}
